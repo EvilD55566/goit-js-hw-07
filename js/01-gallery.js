a46fe64aa1651348bf4b1923caafea326e3f64f1
Aleksandr Pohorelyi <pogorelyaleksandr@gmail.com>
2022-08-14 23:47:23 +0300

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const ImageGallery = document.querySelector(".gallery");
+const galleryItem = createImageGallery(galleryItems);
+ImageGallery.insertAdjacentHTML("beforeend", galleryItem);
+
+function createImageGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href = "${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div > `;
+    })
+    .join("");
+}
+
+ImageGallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const selectedImage = event.target.getAttribute("data-source");
+
+  const instance = basicLightbox.create(`
+    <img src="${selectedImage}">
+`);
+
+  instance.show();
+
+  ImageGallery.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  });
+});
